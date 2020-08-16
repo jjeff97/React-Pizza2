6abbed2410e3a818dc76ad140f46bdc1f935aabc
James Jeffeson <jrjefferson830@gmail.com>
2020-08-15 22:58:09 -0500

    route set up

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -1,16 +1,22 @@
 import React, { Component } from 'react';
 import './App.css';
-import { HashRouter as Router, Route, Link} from 'react-router-dom';
+import { HashRouter as Router, Route, Link } from 'react-router-dom';
 import TypePage from '../Pages/TypePage/TypePage';
+import CustomerPage from '../Pages/CustomerPage/CustomerPage';
+import PizzaPage from '../Pages/PizzaPage/PizzaPage';
+import SummaryPage from '../Pages/SummaryPage/SummaryPage';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <h1>Sal's Famous Pizzaria</h1>
-       < Router>
-       <Route exact path="/" component={TypePage}/>
-       </Router>
+        <Router>
+          <Route exact path="/" component={TypePage} />
+          <Route exact path="/customer" component={CustomerPage} />
+          <Route exact path="/pizza" component={PizzaPage} />
+          <Route exact path="summary" component={SummaryPage} />
+        </Router>
       </div>
     );
   }
