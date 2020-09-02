8faeea76217198d96fcb58ef9cb1f8028a693cbe
James Jeffeson <jrjefferson830@gmail.com>
2020-09-01 22:07:48 -0500

    Reducer setup

--- src/index.js
+++ src/index.js
@@ -2,16 +2,25 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './Components/App/App';
-import * as serviceWorker from './serviceWorker';
+import { createStore } from 'redux';
+import { Provider } from 'react-redux';
 
+const storeInstance = createStore();
 ReactDOM.render(
-  <React.StrictMode>
+  <Provider store={storeInstance}>
     <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+    </Provider>,
+     document.getElementById('root')
 );
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+
+//what does the pizza need?
+//size STRING
+//topping STRING
+/*
+[
+  {
+size: String
+}
+]
+*/
