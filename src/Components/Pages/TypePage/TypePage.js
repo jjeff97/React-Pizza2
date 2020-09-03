75513d3184bebda1326de36f6bb531007dc196d5
James Jeffeson <jrjefferson830@gmail.com>
2020-09-02 21:28:57 -0500

    dispatch build

--- src/Components/Pages/TypePage/TypePage.js
+++ src/Components/Pages/TypePage/TypePage.js
@@ -1,13 +1,20 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
 
 class TypePage extends Component {
-  render() {
+onTypeClick = (type) => (event) =>{
+  console.log(type);
+  this.props.dispatch({type: "SET_ORDER_TYPE", payload: type})
+};
+    render() {
     return (
       <div>
         <h1> Type Page </h1>
+        <button onClick={this.onTypeClick('delivery')}>Deliver</button>
+        <button onClick={this.onTypeClick('carryout')}>Carry Out</button>
       </div>
     );
   }
 }
 
-export default TypePage;
+export default connect()(TypePage);
--- src/index.js
+++ src/index.js
@@ -3,34 +3,35 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './Components/App/App';
 
-import { createStore, combineReducers } from 'redux';
+import { createStore, combineReducers, applyMiddleware } from 'redux';
 import { Provider } from 'react-redux';
+import logger from 'redux-logger';
 //string
-const typeReducer = (state = 'delivery', action)=> {
-if(action.type === "SET_ORDER_TYPE"){
-return action.payload;
-}
-return state;
-}
+const typeReducer = (state = 'delivery', action) => {
+  if (action.type === 'SET_ORDER_TYPE') {
+    return action.payload;
+  }
+  return state;
+};
 
 //Object
-const customerReducer = ()=> {};
+const customerReducer = () => {};
 
 //array
-const pizzaReducer = ()=> {};
+const pizzaReducer = () => {};
 
 const storeInstance = createStore(
   combineReducers({
-typeReducer,
-  }
-  ));
+    typeReducer,
+  }),
+  applyMiddleware(logger)
+);
 ReactDOM.render(
   <Provider store={storeInstance}>
     <App />
-    </Provider>,
-     document.getElementById('root')
+  </Provider>,
+  document.getElementById('root')
 );
-
 
 //what does the pizza need?
 //size STRING
