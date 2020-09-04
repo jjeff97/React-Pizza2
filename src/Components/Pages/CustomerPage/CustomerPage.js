4a0bb530d04b44ac4bae12170cf79c92024407e1
James Jeffeson <jrjefferson830@gmail.com>
2020-09-03 22:44:07 -0500

    condtional rendering of input fields

--- src/Components/Pages/CustomerPage/CustomerPage.js
+++ src/Components/Pages/CustomerPage/CustomerPage.js
@@ -2,38 +2,46 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 
 class CustomerPage extends Component {
-
   state = {
-    fname: "",
-    lname: "",
-    address: ""
-  }
-  onInputChange = (input) => (event) => {
-this.setState({
-  [input]: event.target.value,
-});
+    fname: '',
+    lname: '',
+    address: '',
   };
+  onInputChange = (input) => (event) => {
+    this.setState({
+      [input]: event.target.value,
+    });
+  };
+
   render() {
+    console.log(this.props);
     return (
       <div>
         <h1> Customer Page </h1>
-        <input type="text" onChange={this.onInputChange('fname')}
-         placeholder="Enter First Name"
+        <input
+          type="text"
+          onChange={this.onInputChange('fname')}
+          placeholder="Enter First Name"
+        />
+
+        <input
+          type="text"
+          onChange={this.onInputChange('lname')}
+          placeholder="Enter Last Name"
+        />
+
+        {this.props.store.typeReducer === 'delivery' && (
+          <input
+            type="text"
+            onChange={this.onInputChange('address')}
+            placeholder="Enter Address"
           />
-
-        <input type="text" onChange={this.onInputChange('lname')}
-         placeholder="Enter Last Name"
-          />
-
-        <input type="text" onChange={this.onInputChange('address')} 
-        placeholder="Enter Address"
-         />
-
+        )}
       </div>
     );
   }
 }
 
-const mapStoreToProps = (store) => ({store});
+const mapStoreToProps = (store) => ({ store });
 
-export default connect()(CustomerPage);
+export default connect(mapStoreToProps)(CustomerPage);
