d06e0f232b91a3832a43a35ebb6067cc1d45f0a7
James Jeffeson <jrjefferson830@gmail.com>
2020-09-03 22:16:07 -0500

    customer page build

--- src/Components/Pages/CustomerPage/CustomerPage.js
+++ src/Components/Pages/CustomerPage/CustomerPage.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
 
 class CustomerPage extends Component {
 
@@ -8,20 +9,31 @@ class CustomerPage extends Component {
     address: ""
   }
   onInputChange = (input) => (event) => {
-
-  }
+this.setState({
+  [input]: event.target.value,
+});
+  };
   render() {
     return (
       <div>
         <h1> Customer Page </h1>
-        <input type="text" onChange={this.onInputChange('fname')} placeholder="Enter First Name" />
-        <input type="text" onChange={this.onInputChange('lname')} placeholder="Enter Last Name" />
+        <input type="text" onChange={this.onInputChange('fname')}
+         placeholder="Enter First Name"
+          />
 
-        <input type="text" onChange={this.onInputChange('address')} placeholder="Enter Address" />
+        <input type="text" onChange={this.onInputChange('lname')}
+         placeholder="Enter Last Name"
+          />
+
+        <input type="text" onChange={this.onInputChange('address')} 
+        placeholder="Enter Address"
+         />
 
       </div>
     );
   }
 }
 
-export default CustomerPage;
+const mapStoreToProps = (store) => ({store});
+
+export default connect()(CustomerPage);
