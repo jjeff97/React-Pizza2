7c26ecc6501671f80f72cb6db73fd515e87d1acc
James Jeffeson <jrjefferson830@gmail.com>
2020-09-15 22:09:28 -0500

    summary page

--- src/Components/Pages/SummaryPage/SummaryPage.js
+++ src/Components/Pages/SummaryPage/SummaryPage.js
@@ -1,13 +1,25 @@
 import React, { Component } from 'react';
+import { connect} from 'react-redux';
+
 
 class SummaryPage extends Component {
   render() {
+    console.log(this.props);
+
     return (
       <div>
         <h1> Summary Page </h1>
+        <div>
+          <p>{this.props.store.customerReducer.fname}</p>
+          <p>{this.props.store.customerReducer.lname}</p>
+          <p>{this.props.store.customerReducer.address}</p>
+
+        </div>
       </div>
     );
   }
 }
 
-export default SummaryPage;
+const mapStoreToProps = (store) => ({store});
+
+export default connect(mapStoreToProps)(SummaryPage);
